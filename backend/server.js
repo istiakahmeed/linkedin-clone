8d837c4b0488abe30ebb0a0ccf85add1808e1b90
Istiak Ahmed <istiakahmed6219@gmail.com>
2024-10-20 12:46:13 +0600

    add userroute

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
-
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
@@ -10,8 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
