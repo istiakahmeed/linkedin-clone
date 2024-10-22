01b4b93483abf916dea273ea1a128d3d0be69512
Istiak Ahmed <istiakahmed6219@gmail.com>
2024-10-22 16:18:52 +0600

    complete the user model

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,76 @@
+import User from "../models/user.model.js";
+
+export const getSuggestedConnections = async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.user_id).select("connections");
+
+    // find users who are not already connected, and also do not recommend our own profile !!
+    const suggestedUser = await User.findById({
+      _id: {
+        $ne: req.user._id,
+        $nin: currentUser.connections,
+      },
+    })
+      .select("name username profilePicture headline")
+      .limit(3);
+    res.json(suggestedUser);
+  } catch (error) {
+    console.error("Error in getSuggestedConnections controller:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getPublicProfile = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      "-password"
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.log("Error in getPublicProfile controller:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const allowedFields = [
+      "name",
+      "username",
+      "headline",
+      "about",
+      "location",
+      "profilePicture",
+      "bannerImg",
+      "skills",
+      "experience",
+      "education",
+    ];
+
+    const updatedData = {};
+
+    for (const field of allowedFields) {
+      if (req.body[field]) {
+        updatedData[field] = req.body[field];
+      }
+    }
+
+    // todo: check for the profile img and banner img => upload to cloudinary
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { $set: updatedData },
+      { new: true }
+    ).select("-password");
+
+    res.json(user);
+  } catch (error) {
+    console.log("Error in updateProfile controller:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
